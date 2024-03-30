f95813c6e703232b37ae815599ca5b91d025f9cb
Son Do <dominhson580@gmail.com>
2024-03-30 23:25:41 +0700

    Fix data size converter

--- src/utils/data-size.util.ts
+++ src/utils/data-size.util.ts
@@ -2,6 +2,10 @@ import { MEMORY_UNITS, MemoryUnit } from "@root/constants/unit.const";
 
 export class DataSizeUtil {
   static convert(value: number, from: MemoryUnit, to: MemoryUnit): number {
+    if (value < 0) {
+      throw new Error("Negative memory value");
+    }
+
     const fromIndex = MEMORY_UNITS.indexOf(from);
     const toIndex = MEMORY_UNITS.indexOf(to);
 
@@ -9,7 +13,7 @@ export class DataSizeUtil {
       throw new Error("Invalid memory unit");
     }
 
-    const multiplier = 1024 ** (toIndex - fromIndex);
+    const multiplier = 1024 ** (fromIndex - toIndex);
     return value * multiplier;
   }
 }
